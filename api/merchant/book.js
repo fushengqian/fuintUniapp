04403dcdba361984a64df8e24cc8827cd2006f5b
fushengqian <fushengqian@qq.com>
2025-08-07 08:53:45 +0800

    fixed 分类页面滚动、商户预约管理等页面优化

--- api/merchant/book.js
+++ api/merchant/book.js
@@ -2,15 +2,15 @@ import request from '@/utils/request'
 
 // api地址
 const api = {
-  bookList: 'clientApi/merchant/book/list',
-  bookDetail: 'clientApi/merchant/book/detail',
-  cancel: 'clientApi/merchant/book/cancel',
-  confirm: 'clientApi/merchant/book/confirm',
+  bookList: 'merchantApi/book/list',
+  bookDetail: 'merchantApi/book/detail',
+  cancel: 'merchantApi/book/cancel',
+  confirm: 'merchantApi/book/confirm'
 }
 
 // 我的预约列表
 export const bookList = (param) => {
-  return request.get(api.bookList, param)
+  return request.post(api.bookList, param)
 }
 
 // 我的预约详情
@@ -20,10 +20,10 @@ export const bookDetail = (bookId) => {
 
 // 取消预约
 export function cancel(bookId, data) {
-  return request.get(api.cancel, { bookId, ...data })
+  return request.post(api.cancel, { bookId, ...data })
 }
 
 // 确认预约
-export function confirm(bookId, data) {
-  return request.get(api.confirm, { bookId, ...data })
+export function confirm(bookId, status) {
+  return request.post(api.confirm, { bookId, status })
 }
